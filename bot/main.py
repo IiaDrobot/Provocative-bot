068198d5bd48c400187c8e1bda027dce04e6bc87
Iia Drobot <ija-d@ukr.net>
2026-04-05 17:31:29 +0200

    version of bot a

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,12 @@
 import os
-
-
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler
-
 from bot.handlers import start, handle_buttons
-
-
-
-
 
 def main():
     bot_token = os.getenv("BOT_TOKEN")
 
     if not bot_token:
-        raise ValueError("BOT_TOKEN не найден в .env")
+        raise ValueError("BOT_TOKEN не найден!Проверь Railway Variables")
 
     app = Application.builder().token(bot_token).build()
 
@@ -24,7 +17,6 @@ def main():
     print("Бот запущен...")
 
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
